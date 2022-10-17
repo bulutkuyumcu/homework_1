5e87622c3e10b5e152884bb8e72f59366fd3749d
bulutkuyumcu <bulut.kuyumcu@gmail.com>
2022-10-17 21:17:17 +0300

    Initial commit

---
+++ homework_first.py
@@ -0,0 +1,40 @@
+urunler = []
+fiyatlar = []
+urunler.append('sucuk')
+urunler.append('peynir')
+urunler.append('yumurta')
+urunler.append('sosis')
+urunler.append('ekmek')
+
+fiyatlar.append(100)
+fiyatlar.append(70)
+fiyatlar.append(30)
+fiyatlar.append(50)
+fiyatlar.append(10)
+
+print(urunler)
+print(fiyatlar)
+
+print(urunler[0], fiyatlar[0])
+print(urunler[1], fiyatlar[1])
+print(urunler[2], fiyatlar[2])
+print(urunler[3], fiyatlar[3])
+print(urunler[4], fiyatlar[4])
+
+
+
+urunler_ve_fiyatları = []
+
+
+urunler_ve_fiyatları.append('sucuk')
+urunler_ve_fiyatları.append(100)
+urunler_ve_fiyatları.append('peynir')
+urunler_ve_fiyatları.append(70)
+urunler_ve_fiyatları.append('yumurta')
+urunler_ve_fiyatları.append(30)
+urunler_ve_fiyatları.append('sosis')
+urunler_ve_fiyatları.append(50)
+urunler_ve_fiyatları.append('ekmek')
+urunler_ve_fiyatları.append(10)
+
+print(urunler_ve_fiyatları)
